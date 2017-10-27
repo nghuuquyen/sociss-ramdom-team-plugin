2a6da3b1ff90b8d20eb32e6e9df7869adc273f0d
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2017-10-27 12:27:07 +0700

    Update CORS configs

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.set('view engine', 'server.view.html');
 app.set('views', __dirname + '/app/views');
 
 function allowCrossDomain(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+    	
     next();
 }
 
